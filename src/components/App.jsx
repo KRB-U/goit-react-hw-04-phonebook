2849b1446b53811815637a3d5f61af6b466ecc0c
unknown <rubikonspace@ukr.net>
2023-10-17 17:05:58 +0300

    3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,24 +11,22 @@ import 'components/ContactForm/ContactForm';
 import { TitlePhonebook, TitleContacts, ContainerWrapper } from './App.styled';
 
 const App = () => {
-  const [contacts, setContacts] = useState([
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    { id: 'id-6', name: 'Emma Thompson', number: '987-65-43' },
-    { id: 'id-7', name: 'Oliver Davis', number: '876-54-32' },
-  ]);
-  const [filter, setFilter] = useState('');
-
-  useEffect(() => {
+  const [contacts, setContacts] = useState(() => {
     const contactsLs = localStorage.getItem('contacts');
     const parseContacts = JSON.parse(contactsLs);
 
-    if (parseContacts !== null) {
-      setContacts(parseContacts);
-    }
-  }, []);
+    return (
+      parseContacts || [
+        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+        { id: 'id-6', name: 'Emma Thompson', number: '987-65-43' },
+        { id: 'id-7', name: 'Oliver Davis', number: '876-54-32' },
+      ]
+    );
+  });
+  const [filter, setFilter] = useState('');
 
   useEffect(() => {
     localStorage.setItem('contacts', JSON.stringify(contacts));
